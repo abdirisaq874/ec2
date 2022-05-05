b508d6810e0ab6d2b5ff10d801548ba9ae3332c9
mandeez <abdirisaq874@gmail.com>
2022-05-05 15:07:26 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const address = search.value
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector("#message-2")
+
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ""
+    if(!search.value){
+        messageOne.textContent = "you should provide an address"
+    }else{
+        fetch("http://localhost:3000/weather?address="+ search.value).then((response)=>{
+    response.json().then((data)=>{
+        if(data.Error){
+            messageOne.textContent = ""
+            messageTwo.textContent= data.Error
+        }else{
+            messageOne.textContent = ""
+            messageTwo.textContent = data.msg
+        }
+    })
+    })
+    }
+    
+
+})
---
+++ src/app.js
@@ -0,0 +1,106 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require("../../weather-app/utils/geocode")
+const forecast = require('../../weather-app/utils/forecast')
+const { json } = require('express')
+
+
+const app = express()
+
+// set handlebars engine and views location
+app.set('view engine',"hbs")
+app.set('views',path.join(__dirname,"../templates/views"))
+hbs.registerPartials(path.join(__dirname,'../templates/partials'))
+
+// set up static directory to serve
+app.use(express.static(path.join(__dirname, "../public")))
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'weather App',
+        name : 'mandeez'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'help'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title : "about Me"
+    })
+})
+
+app.get('',(req,res)=>{
+    res.send('<h1>hello express!</h1>')
+})
+
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send("you should provide an address")
+    }else{
+        
+            geocode(req.query.address,(error,{data}={})=>{
+                if(error)
+                {
+                    return res.send(JSON.stringify({ Error :  error}))
+                } else
+                {
+                    forecast(data.center[0],data.center[1],(error,data1)=>{
+                    if(error)
+                    {
+                        return res.send(JSON.stringify({
+                            Error: error
+                        }))
+                    }
+                    else
+                    {
+                        const send ={
+                            longtitude : data.center[1],
+                            latitude : data.center[0],
+                            location : data.place_name,
+                            msg : data1
+                        }
+                        return res.send(JSON.stringify(send))
+                    }
+                })
+                
+                }
+            })
+    }
+})
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error : 'you must provide a search term'
+        })
+    }else {
+        res.send({
+            products:[]
+        })
+    }
+    
+})
+app.get('/help/*',(req,res)=>{
+    res.render('404_page',{
+        title:"404 page",
+        error: "Help article not found"
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404_page',{
+        title: "404 page",
+        error: "Page not found"
+    })
+})
+
+app.listen(3000,()=>{
+    ("server is up on port 3000.")
+})
